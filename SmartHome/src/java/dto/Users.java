f8681c4817693ba9664394b3d4bc00928ebc3e01
nmt <nmthuan0321@gmail.com>
2026-02-27 13:33:36 +0700

    DTO User

---
+++ SmartHome/src/java/dto/Users.java
@@ -0,0 +1,14 @@
+package dto;
+
+import java.time.LocalDate;
+
+public class Users {
+
+  private String id;
+  private String username;
+  private String password;
+  private String fullName;
+  private String email;
+  private String role;
+  private LocalDate createAt;
+}
